cc4220897967c578c992ad4d3f77b02a039e43c5
Viljami Ranta <enderledark@gmail.com>
2020-06-25 16:57:58 +0300

    Update prnt.sql

--- prnt.sql
+++ prnt.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5deb2
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: 25.06.2020 klo 16:56
--- Palvelimen versio: 8.0.20-0ubuntu0.20.04.1
--- PHP Version: 7.4.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -17,16 +8,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `prnt`
---
-
--- --------------------------------------------------------
-
---
--- Rakenne taululle `scores`
---
 
 CREATE TABLE `scores` (
   `code` varchar(10) NOT NULL,
